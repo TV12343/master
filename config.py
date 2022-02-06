a3c70df920b4be8827baff09545f1e1e3f3e9512
Tatyana Vedenicheva <die873@gmail.com>
2022-02-06 21:24:19 +0300

    последняя версия проекта

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'mysql://tatyana:1234@localhost/rating'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
